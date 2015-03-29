49a8744439b1312a942a9fea13c1faaf102d36ba
Alexander Malaev <scream@spuge.net>
2015-03-29 12:07:27 +0300

    Add markdown for compulsory description

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,15 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider => 'Google',
-      :google_storage_access_key_id => Rails.application.secrets.google_storage_key,
-      :google_storage_secret_access_key => Rails.application.secrets.google_storage_secret
-  }
 
-  config.fog_directory = Rails.application.secrets.fog_directory
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+        :provider => 'Google',
+        :google_storage_access_key_id => Rails.application.secrets.google_storage_key,
+        :google_storage_secret_access_key => Rails.application.secrets.google_storage_secret
+    }
+
+    config.fog_directory = Rails.application.secrets.fog_directory
+  end
 end
